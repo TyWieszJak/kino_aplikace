643c7e8909b237fc7729e4aece8166c7b26b4c4d
Mara <marekdarek646@gmail.com>
2024-12-29 13:51:35 +0100

    add : forms,urls,views and 2 templates

--- kino_aplikace/rezervace/views.py
+++ kino_aplikace/rezervace/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ReservationForm
+from.models import Movie
 
-# Create your views here.
+
+def index(request):
+    movies = (Movie.objects.all())  # Získání všech filmů z databáze
+    return render(request, 'reservation/index.html', {'movies': movies})
+
+
+def reservation(request):
+    if request.method == "POST":
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = ReservationForm()
+
+    return render(request, 'reservation/reservation.html', {'form': form})
